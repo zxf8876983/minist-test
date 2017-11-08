6c60c20bd8459b369dc09fb2b74555fa4cc0d146
zxf8876983 <zxf8876983@163.com>
2017-11-08 16:20:00 +0800

    model-7

--- create_model_7.py
+++ create_model_7.py
@@ -56,8 +56,8 @@ def max_pool_2x2(x):
                         strides=[1, 2, 2, 1], padding='SAME')
 
 
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+W_conv1 = weight_variable([5, 5, 1, 1])
+b_conv1 = bias_variable([1])
 
 x_image = tf.reshape(x, [-1,28,28,1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
@@ -65,24 +65,24 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([5, 5, 1, 1])
+b_conv2 = bias_variable([1])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 #h_conv2 = conv2d(h_pool1, W_conv2) + b_conv2
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
+W_fc1 = weight_variable([7 * 7, 49])
+b_fc1 = bias_variable([49])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 #h_fc1 = tf.matmul(h_pool2_flat, W_fc1) + b_fc1
 
 #keep_prob = tf.placeholder(tf.float32)
 #h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, 10])
+W_fc2 = weight_variable([49, 10])
 b_fc2 = bias_variable([10])
 
 #y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
@@ -115,7 +115,7 @@ for i in range(20000):
     #train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
     train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
-save_path = saver.save(sess, "my_net/model_3/model3.ckpt")
+save_path = saver.save(sess, "my_net/model_7/model7.ckpt")
 print ("Model saved in file: ", save_path)
 
 print("test accuracy %g"%accuracy.eval(feed_dict={
